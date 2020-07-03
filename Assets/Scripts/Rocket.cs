735f2cb19824ca77130ce9091f062299e8b3f282
Simon Cuddihy <simoncuddihy@gmail.com>
2020-07-03 01:41:53 +0100

    Rocket boost game - just the basics

---
+++ Assets/Scripts/Rocket.cs
@@ -0,0 +1,74 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rocket : MonoBehaviour
+{
+
+    Rigidbody rigidbody;
+    [SerializeField] float rcsThrust = 150f;
+    [SerializeField] float mainThrust = 20f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody>(); 
+        rigidbody.mass = 1;
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        Thrust();
+        Rotate();
+    }
+
+    private void Thrust()
+    {
+        if (Input.GetKey(KeyCode.Space))
+        {
+            rigidbody.AddRelativeForce(Vector3.up * mainThrust);
+        }
+    }
+
+    private void Rotate()
+    {
+        rigidbody.freezeRotation = true; // take manual control of rotation
+
+        float rotationThisFrame = rcsThrust * Time.deltaTime;
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            transform.Rotate(Vector3.forward * rotationThisFrame);
+        }
+
+        else if (Input.GetKey(KeyCode.D))
+        {
+            transform.Rotate(-Vector3.forward * rotationThisFrame);
+        }
+
+        rigidbody.freezeRotation = false; // resume physics control of rotation
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        switch (collision.gameObject.tag)
+        {
+            case "friendly":
+                print("ok");
+                break;
+            case "fuel":
+                print("refueling");
+                break;
+            default:
+                print("dead");
+                break;
+        }
+        //ContactPoint contact = collision.contacts[0];
+        //Quaternion rotation = Quaternion.FromToRotation(Vector3.up, contact.normal);
+        //Vector3 position = contact.point;
+        //Instantiate(explosionPrefab, position, rotation);
+        //Destroy(gameObject);
+    }
+}
